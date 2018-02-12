beae52821d6cba90bde4bc7f1ed3b05744d4101c
srohimah <sitirohimach@gmail.com>
2018-02-12 13:16:13 +0700

    release 1

--- parser.js
+++ parser.js
@@ -1,25 +1,66 @@
 "use strict"
 
+const fs = require('fs')
+
 class Person {
-  // Look at the above CSV file
-  // What attributes should a Person object have?
+  constructor (id,first_name,last_name,email,phone,created_at){
+    this.id = id
+    this.first_name = first_name
+    this.last_name = last_name
+    this.email = email
+    this.phone = phone
+    this.created_at = created_at
+  }
 }
 
 class PersonParser {
 
   constructor(file) {
     this._file = file
-    this._people = null
+    this._people = []
   }
 
   get people() {
+    let data = fs.readFileSync(this._file,'utf8')
+    let rowData = data.split('\n')
+    for(let i=1; i<rowData.length; i++){
+      let personData = rowData[i].split(',')
+      let objPerson = new Person(personData[0],personData[1],personData[2],personData[3],personData[4],personData[5])
+      this._people.push(objPerson)
+    }
     return this._people
   }
 
-  addPerson() {}
+  addPerson(newPerson) {
+    this._people.push(newPerson)
+    return this._people
+  }
+
+  save(){
+    let str = ''
+    str+=Object.keys(this._people[0])
+      for(let i =0; i<this._people.length; i++){
+        let arr = []      
+        if(this._people[i].id !=='' && this._people[i].first_name !=='' && this._people[i].last_name !=='' && this._people[i].email !=='' && this._people[i].phone !=='' && this._people[i].created_at !==''){        
+          arr.push(this._people[i].id)
+          arr.push(this._people[i].first_name)
+          arr.push(this._people[i].last_name)
+          arr.push(this._people[i].email)
+          arr.push(this._people[i].phone)
+          arr.push(this._people[i].created_at)
+          str+='\n'+arr
+        }      
+    }
+    fs.writeFileSync('people.csv',str,'utf8')
+    return str
+    
+  }
 
 }
 
 let parser = new PersonParser('people.csv')
+parser.people
+parser.addPerson(new Person('6','Jingga','Suseno','jinggaSu@example.com','8-12-54345-2343', new Date()))
+parser.save()
 
 console.log(`There are ${parser.people.size} people in the file '${parser.file}'.`)
