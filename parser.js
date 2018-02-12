e47c6d577d307b288b8be26b3c331fea7ec4cdcb
Reza Pramudhika <rezapramudhika@gmail.com>
2018-02-12 15:42:20 +0700

    Release 0

--- parser.js
+++ parser.js
@@ -1,6 +1,66 @@
 "use strict"
 
+const fs = require('fs');
+
 class Person {
+
+  constructor(id, firstName, lastName, email, phone, createdAt){
+    this._id = id;
+    this._firstName = firstName;
+    this._lastName = lastName;
+    this._email = email;
+    this._phone = phone;
+    this._createdAt = createdAt;
+  }
+
+  set id(value){
+    this._id = value;
+  }
+
+  get id(){
+    return this._id;
+  }
+
+  set firstName(value){
+    this._firstName = value;
+  }
+
+  get firstName(){
+    return this._firstName;
+  }
+
+  set lastName(value){
+    this._lastName = value;
+  }
+
+  get lastName(){
+    return this._lastName;
+  }
+
+  set email(value){
+    this._email = value;
+  }
+
+  get email(){
+    return this._email;
+  }
+
+  set phone(value){
+    this._phone = value;
+  }
+
+  get phone(){
+    return this._phone;
+  }
+
+  set createdAt(value){
+    this._createdAt = value;
+  }
+
+  get createdAt(){
+    return this._createdAt;
+  }
+
   // Look at the above CSV file
   // What attributes should a Person object have?
 }
@@ -9,17 +69,30 @@ class PersonParser {
 
   constructor(file) {
     this._file = file
-    this._people = null
+    this._people = []
+    this.data = this.readData();
+    this.person = new Person();
+  }
+
+  readData(){
+    var data = fs.readFileSync(this._file, 'UTF-8');
+    return data.split('\n');
   }
 
   get people() {
-    return this._people
+    for(let i=1; i<this.data.length; i++){
+      let row = this.data[i].split(',')
+      let person = new Person(row[0], row[1], row[2], row[3], row[4], row[5]);
+      this._people.push(person)
+    }
+    return this._people;
   }
 
   addPerson() {}
 
 }
 
-let parser = new PersonParser('people.csv')
-
-console.log(`There are ${parser.people.size} people in the file '${parser.file}'.`)
+let parser = new PersonParser('./people.csv');
+console.log(parser.readData());
+console.log(parser.people);
+// console.log(`There are ${parser.people.length} people in the file '${parser.file}'.`);
