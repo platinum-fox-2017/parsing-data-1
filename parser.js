1e71e30af3324352b1ee6ae18c03aa44bc122190
Fachrizal Gita <Cecha@Cechas-MacBook-Pro.local>
2018-02-12 14:09:05 +0700

    done

--- parser.js
+++ parser.js
@@ -18,30 +18,52 @@ class Person {
 class PersonParser {
 
   constructor(file) {
-    this._file = fs.readFileSync('./people.csv', 'utf8')
+    this._file = file
     this._people = null
     this.data = this.getData()
   }
   getData(){
-    let arr = this._file.toString().split('\n')
+    let data = fs.readFileSync('./people.csv', 'utf8').trim()
+    let arr = data.toString().split('\n')
     let result = []
     for(let i=1; i<arr.length; i++){
-      let arrPeople = arr[i].split(',')
-      let person = new Person(arrPeople)
+      let arrPerson = arr[i].split(',')
+      let person = new Person(arrPerson)
       result.push(person)
     }
-
+    this._people = result
     return result
   }
   get people() {
     return this._people
   }
 
-  addPerson() {}
+  addPerson(id, first_name, last_name, email, phone, created_at) {
+    let data = fs.readFileSync('./people.csv', 'utf8').trim()
+    let arr = data.toString().split('\n')
+    let result = []
+    for(let i=0; i<arr.length; i++){
+      let arrPerson = arr[i].split(',')
+      result.push(arrPerson)
+    }
+    let newData = []
+    newData.push(id, first_name, last_name, email, phone, created_at)
+    result.push(newData)
+    this._people = result
+
+    return this._people[this._people.length-1]
+  }
+  save(){
+    let dataStr = this._people.join('\n')
+    return fs.writeFileSync('people.csv', dataStr, 'utf8')
+  }
+  get size(){
+    return this._people.length
+  }
 
 }
 
 let parser = new PersonParser('people.csv')
-console.log(parser.getData());
-
-// console.log(`There are ${parser.people.size} people in the file '${parser.file}'.`)
+// parser.addPerson('202', 'Valensio', 'Deva', 'devdots@gmail.com', '081398220030', Date.now())
+// parser.save();
+console.log(`There are ${parser.size} people in the file '${parser._file}'.`)
